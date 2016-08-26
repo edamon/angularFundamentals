67db0a53c8c0cbe072f163323f139c970e640dda
Eric Damon <eric.damon@gmail.com>
2016-08-25 20:42:40 -0500

    add built-in and custom filters to ng-repeat, title text.

--- directives-lesson/directives-lesson.module.js
+++ directives-lesson/directives-lesson.module.js
@@ -1,1 +1,6 @@
-angular.module('sn-directives-lesson', []);
+angular.module('sn-directives-lesson', [])
+    .filter('reverse', function() {
+        return function (text) {
+            return text.split("").reverse().join("");
+        };
+    });
--- directives-lesson/page2.controller.js
+++ directives-lesson/page2.controller.js
@@ -2,10 +2,13 @@
 'use strict';
     module.controller('Page2Ctrl', Page2Ctrl);
 
-    function Page2Ctrl(){
+    function Page2Ctrl($filter, reverseFilter){
+        // to inject a custom filter, add the word 'Filter' after the filter name
         var self = this;
-        self.title = 'try adding duplicates, then check console error';
-        self.people = [
+        self.title = 'Built in Filters with ng-repeat';
+        //todo: apply the reverse filter in the controller instead of template
+        //self.title = reverseFilter(self.title);
+        var people = [
             {
                 first : 'Eric',
                 last: 'Damon'
@@ -13,11 +16,21 @@
             {
                 first : 'Brad',
                 last: 'Zepecki'
+            },
+            {
+                first : 'James',
+                last: 'Newell'
+            },
+            {
+                first : 'Justin',
+                last: 'Bowers'
             }
         ];
+        //use the $filter service in a controller
+        self.people = $filter('limitTo')(people, 3);
 
-        self.addPerson = function (person) {
-            self.people.push(person);
+        self.addPerson = function (first, last) {
+            self.people.push({first: first, last: last});
         }
     }
 })(angular.module('sn-directives-lesson'));
